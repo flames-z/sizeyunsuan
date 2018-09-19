1bce1988367bf74ee37daa1a1bcf67497749b8e5
3216005169 <1151899508@qq.com>
2018-09-19 20:22:33 +0800

    initial

---
+++ src/team/zw/sizeyunsuan/calculate.java
@@ -0,0 +1,5 @@
+package team.zw.sizeyunsuan;
+
+public class calculate {
+	
+}
---
+++ src/team/zw/sizeyunsuan/sizeyunsuan.java
@@ -0,0 +1,27 @@
+package team.zw.sizeyunsuan;
+
+import java.util.Scanner;
+
+public class sizeyunsuan {
+	public static void main(String[] args) {
+//		while(true) {
+			System.out.println("\n--------------------伍欣怡&钟秋爽 结对程序----------------------");
+	        System.out.println("                                                           ");
+	        System.out.println("            -n 题目个数                ");
+	        System.out.println("            -r 题目中数值（自然数、真分数和真分数分母）的范围                ");
+	        System.out.println("                                                           ");
+	        System.out.println("        例：-n 10 -r 10                     ");
+	        System.out.println("-------------------------------------------------------------");
+	        
+	        System.out.println("请输入命令： ");
+	        Scanner command = new Scanner(System.in);
+	        
+	        String titleNum = command.nextLine().split(" ")[1];
+	        String numRange = command.nextLine().split(" ")[1];
+	        
+	        System.out.println(titleNum);
+	        System.out.println(numRange);
+	        
+//		}
+	}
+}
