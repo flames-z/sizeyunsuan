10482c6522a938596089405fc758a6565b70172f
3216005172 <2449230425@qq.com>
2018-09-25 21:41:15 +0800

    第一次修改

--- src/team/zw/sizeyunsuan/Calculate.java
+++ src/team/zw/sizeyunsuan/Calculate.java
@@ -1,36 +1,51 @@
 package team.zw.sizeyunsuan;
 
 public class Calculate {
-	int numerator;
-	int denominator;
+	int n;//大于1真分数前面的数
+	int ne;//分子
+	int deno;//分母
 	
 	public Calculate() {
 		
 	}
 	
-	public Calculate(int numerator, int denominator) {
-		this.numerator = numerator;
-		this.denominator = denominator;
+	public Calculate(int n,int ne, int deno) {
+		this.n = n;
+		this.ne = ne;
+		this.deno = deno;
 	}
 	
-	public Calculate add(Calculate num1, Calculate num2){
-		int newNumerator = num1.numerator * num2.denominator + num1.denominator * num2.numerator;
-		int newDenominator = num1.denominator * num2.denominator;
-		int g = gcd(newDenominator, newNumerator);
-		
-		return new Calculate(newNumerator/g, newDenominator/g);
+	public Calculate add(Calculate n1, Calculate n2){
+		int newne = n1.ne * n2.deno + n1.deno * n2.ne+n1.n*n1.deno+n2.n*n2.deno;
+		int newdeno = n1.deno * n2.deno;
+		int g = gcd(newdeno, newne);
+		int newn;
+		if(newdeno < newne) {
+			newn = newne/newdeno;
+		}
+		else newn =0;
+		return new Calculate(newn,newne/g-newn*newdeno, newdeno/g);
 		
 	}
 	
-	public Calculate sub(Calculate num1, Calculate num2) {
-		int newNumerator = num1.numerator * num2.denominator - num1.denominator * num2.numerator;
-		int newDenominator = num1.denominator * num2.denominator;
-		int g = gcd(newDenominator, newNumerator);
+	public Calculate sub(Calculate n1, Calculate n2) {
 		
-		return new Calculate(newNumerator/g, newDenominator/g);
+		//判断不出现负数,要在这里实现吗？还是在生成后再判断？
+		/***if(newne<0) {
+			System.out.println("出现负数");
+		}***/
+		int newne = n1.ne * n2.deno+n1.n*n1.deno - n1.deno * n2.ne-n2.n*n2.deno;
+		int newdeno = n1.deno * n2.deno;
+		int g = gcd(newdeno, newne);
+		int newn;
+		if(newdeno < newne) {
+			newn = newne/newdeno;
+		}
+		else newn =0;
+		return new Calculate(newn,newne/g, newdeno/g);
 	}
 	
-	public int gcd(int a, int b) {
+	public int gcd(int a, int b) {   //求最大公因子
 		int m = Math.max(a, b);
 		int n = Math.min(a, b);
 		int r;
--- src/team/zw/sizeyunsuan/Generate.java
+++ src/team/zw/sizeyunsuan/Generate.java
@@ -1,5 +1,69 @@
 package team.zw.sizeyunsuan;
 
-public class Generate {
-
+public class Generate{
+	int n,r;
+	
+	
+	//随机出一个运算符
+	String opch(){
+		int n1 = (int)(Math.random()*4+1);
+		if(n1==1)
+			return "+";
+		else if (n1==2)
+			return "-";
+		else if (n1==3)
+			return "✖";
+		else
+			return "➗";
+	}
+	
+	//随机出一个真分数或自然数
+	void rand() {
+		int n1 =(int)(Math.random()*2);
+		if(n1==0) {
+			System.out.printf("%d",(int) (Math.random()*r));
+		}
+		else {
+			//随机这是一个大于1的分数，还是小于1
+			if ((int)(Math.random()*2)==0) {                   //大于1
+				
+			fraction f2 = new fraction();
+			f2.n = (int) (Math.random()*r);
+			f2.nm = (int) (Math.random()*(r-1))+1;
+			f2.deno = (int) (Math.random()*(r-1))+1;
+			System.out.printf("%d %d%d",f2.n,f2.nm,f2.deno);
+			}
+			else {                                          //小于1
+				fraction f3 = new fraction();
+				f3.n = 0;
+				f3.nm = (int) (Math.random()*(r-1))+1;
+				f3.deno = (int) (Math.random()*(r-1))+1;
+				System.out.printf("%d %d%d",f3.nm,f3.deno);
+				}
+			}
+	}
+	
+	 /**************
+	   void fh1{                                               //只有一个运算符的情况
+	  //报错：void is an invalid type for the variable fh1
+			fraction f1 = rand(10);
+			fraction f2 = rand(10);
+			String s1  =  opch();
+			System.out.printf("e = %d %d/%d   %s  %d %d/%d",f1.n ,f1.nm,f1.deno,s1,
+					f2.n ,f2.nm,f2.deno);   
+	}***************/
+	
+	public static void main(String[] args) {
+		
+		Generate g = new Generate();
+		System.out.printf("e = "); 
+		g.rand();
+		System.out.printf(g.opch());
+		g.rand();
+		}
+}
+class fraction {
+	int n;
+	int nm;
+	int deno;
 }
